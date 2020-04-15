f57ad0d8682b1b596c4fadcf02f5580351ed1e88
Kaio Gomes <kaiogomes.nave@gmail.com>
2020-04-15 05:28:02 -0300

    Show used memory

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const prettySize = require('prettySize')
 
 const freeMemory = prettySize(os.freemem())
 const totalMemory = prettySize(os.totalmem())
+const usedMemory = prettySize(os.totalmem() - os.freemem())
 
 console.log(`Total Memory: ${totalMemory}`)
 console.log(`Free Memory: ${freeMemory}`)
+console.log(`Used Memory: ${usedMemory}`)
